a9d3e533a4af3d9d14e7fe922ff5f16774f62caf
Gracjan Polak <gracjan@scrive.com>
2014-05-06 11:42:17 +0200

    Reindent javascript files

--- chrome-scrive-extension/content_script.js
+++ chrome-scrive-extension/content_script.js
@@ -14,141 +14,143 @@
  */
 
 (function() {
-var pdf = undefined;
+  var pdf = undefined;
 
-setupListeners();
+  setupListeners();
 
-function setupListeners() {
-  chrome.extension.onRequest.addListener(
-    function(request, sender, sendResponse) {
-      if (request.type == 'pdfexistsonpage') {
-        pdf = findEmbeddedPdfInDocument(document, request.savedDataForRequests);
-        if (pdf) {
-          sendResponse(true);
-        } else {
-          sendResponse(false);
+  function setupListeners() {
+    chrome.extension.onRequest.addListener(
+      function(request, sender, sendResponse) {
+        if (request.type == 'pdfexistsonpage') {
+          pdf = findEmbeddedPdfInDocument(document, request.savedDataForRequests);
+          if (pdf) {
+            sendResponse(true);
+          } else {
+            sendResponse(false);
+          }
+        };
+
+        if (request.type == 'printtopaper') {
+          window.print();
         }
-      };
 
-      if (request.type == 'printtopaper') {
-        window.print();
-      }
-
-      if (request.type == 'savedataforrequest') {
+        if (request.type == 'savedataforrequest') {
           window.scriveSavedDataForRequest = request.data;
+        }
+
+        if (request.type == 'printtoesign') {
+          // TODO make this more clever
+          sendPDF(pdf, request, sendResponse);
+        }
       }
-
-      if (request.type == 'printtoesign') {
-        // TODO make this more clever
-        sendPDF(pdf, request, sendResponse);
-      }
-    }
-  );
-};
+    );
+  };
 
 
-function qualifyURL( document, url )
-{
+  function qualifyURL( document, url )
+  {
     // it actually tries to contact the server when using IMG tag... not good!
-/*
-    var img = document.createElement('img');
-    img.src = url; // set string url
-    url = img.src; // get qualified url
-    img.src = null; // no server request
-*/
+    /*
+      var img = document.createElement('img');
+      img.src = url; // set string url
+      url = img.src; // get qualified url
+      img.src = null; // no server request
+    */
     var a = document.createElement('a');
     a.href = url; // set string url
     url = a.href; // get qualified url
     return url;
-}
+  }
 
-/**
- * Look through the DOM and search for PDF's
- * 
- * @return Array The pdfs that were found.
- */
-// TODO handle more than one pdf on the page
-function findEmbeddedPdfInDocument(document,savedDataForRequests)
-{
-  // REFACTOR TO USE querySelector
+  /**
+   * Look through the DOM and search for PDF's
+   * 
+   * @return Array The pdfs that were found.
+   */
+  // TODO handle more than one pdf on the page
+  function findEmbeddedPdfInDocument(document,savedDataForRequests)
+  {
+    // REFACTOR TO USE querySelector
     var elems = document.getElementsByTagName("*");
     var count = elems.length;
     for( var i = 0; i<count; i++ ) {
-        var elem = elems[i];
-        var tagName = elem.tagName.toLowerCase();
+      var elem = elems[i];
+      var tagName = elem.tagName.toLowerCase();
 
-        if( tagName=="embed" ) {
-            var xsrc1 = elem.getAttribute("src")
-            var xsrc = qualifyURL(document, xsrc1);
-            var data = savedDataForRequests[xsrc];
-            if( data ) {
-                for( var k in data.responseHeaders ) {
-                    var headerLine = data.responseHeaders[k];
-                    if( headerLine.name.toLowerCase()=="content-type" &&
-                        headerLine.value.search("application/pdf")>=0 ) {
-                        return elem;
-                    }
-                }
+      if( tagName=="embed" ) {
+        var xsrc1 = elem.getAttribute("src")
+        var xsrc = qualifyURL(document, xsrc1);
+        var data = savedDataForRequests[xsrc];
+        if( data ) {
+          for( var k in data.responseHeaders ) {
+            var headerLine = data.responseHeaders[k];
+            if( headerLine.name.toLowerCase()=="content-type" &&
+                headerLine.value.search("application/pdf")>=0 ) {
+              return elem;
             }
-        }
-        else if( tagName=="iframe" || tagName=="frame") {
-          try {
-            var elem = findEmbeddedPdfInDocument(elem.contentDocument,savedDataForRequests);
-            if( elem ) {
-                return elem;
-            }
-          } catch (e) {
-            // this happens when unallowed frame traversals are done
-            console.log("error while traversing frames", e);
           }
         }
+      }
+      else if( tagName=="iframe" || tagName=="frame") {
+        try {
+          var elem = findEmbeddedPdfInDocument(elem.contentDocument,savedDataForRequests);
+          if( elem ) {
+            return elem;
+          }
+        } catch (e) {
+          // this happens when unallowed frame traversals are done
+          console.log("error while traversing frames", e);
+        }
+      }
     }
     return;
-}
-
-/*
- * First get the PDF with a xhr call, then put it to the middleware
- */
-function sendPDF(pdf, request, errorCallback) {
-
-  var src = pdf.getAttribute('src');
-  var savedData = request.savedDataForRequests[src];
-
-  var getpdfXHR = new XMLHttpRequest();
-  getpdfXHR.onload = function() {
-    if (getpdfXHR.status >= 200 && getpdfXHR.status <= 299) {
-      uploadPDFData(getpdfXHR.response, errorCallback);
-    }
-  };
-  getpdfXHR.onerror = function() {
-    errorCallback({
-      'type': 'error',
-      'headers': getpdfXHR.getAllResponseHeaders().split("\n").filter(function(x) { return x!=""; }),
-      'status':  getpdfXHR.status,
-      'response': getpdfXHR.response,
-      'statusText': getpdfXHR.statusText
-    });
-  };
-  if( savedData && savedData.requestBody && savedData.requestBody.formData ) {
-    var formData = new FormData();
-    for(var k in savedData.requestBody.formData) {
-        formData.append(k, savedData.requestBody.formData[k][0]);
-    }
-    getpdfXHR.open("POST", src);
-    getpdfXHR.responseType = "blob";
-    getpdfXHR.send(formData);
   }
-  else {
-    getpdfXHR.open("GET", src);
-    getpdfXHR.responseType = "blob";
-    getpdfXHR.send();
-  }
+
   /*
-   * I'm not sure what is the real difference between 'blob' and
-   * 'arraybuffer', they look similar enough to me. 'Blob' has
-   * mime type associated with it, ArrayBuffer does not.
+   * First get the PDF with a xhr call, then put it to the middleware
    */
-}
+  function sendPDF(pdf, request, errorCallback) {
+
+    var src = pdf.getAttribute('src');
+    var savedData = request.savedDataForRequests[src];
+
+    var getpdfXHR = new XMLHttpRequest();
+    getpdfXHR.onload = function() {
+      if (getpdfXHR.status >= 200 && getpdfXHR.status <= 299) {
+        uploadPDFData(getpdfXHR.response, errorCallback);
+      }
+    };
+    getpdfXHR.onerror = function() {
+      errorCallback({
+        'type': 'error',
+        'headers': getpdfXHR.getAllResponseHeaders().split("\n").filter(function(x) { return x!=""; }),
+        'status':  getpdfXHR.status,
+        'response': getpdfXHR.response,
+        'statusText': getpdfXHR.statusText
+      });
+    };
+    if( savedData && savedData.requestBody && savedData.requestBody.formData ) {
+      var formData = new FormData();
+      for(var k in savedData.requestBody.formData) {
+        formData.append(k, savedData.requestBody.formData[k][0]);
+      }
+      getpdfXHR.open("POST", src);
+      getpdfXHR.responseType = "blob";
+      getpdfXHR.send(formData);
+    }
+    else {
+      getpdfXHR.open("GET", src);
+      getpdfXHR.responseType = "blob";
+      getpdfXHR.send();
+    }
+    /*
+     * I'm not sure what is the real difference between 'blob' and
+     * 'arraybuffer', they look similar enough to me. 'Blob' has mime
+     * type associated with it, ArrayBuffer does not. ArrayBuffer
+     * seems deprecated in favor of ArrayBufferView, but
+     * ArrayBufferView is not legal here.
+     */
+  }
 
 
 })();
@@ -156,7 +158,7 @@ function sendPDF(pdf, request, errorCallback) {
 function uploadPDFData(data, errorCallback, sameWindow) {
   var xmlHttpRequestPUT = new XMLHttpRequest();
   xmlHttpRequestPUT.onload = function () {
-    if( xmlHttpRequestPUT.status == 200 ) {
+    if( xmlHttpRequestPUT.status >= 200 && xmlHttpRequestPUT.status <=299 ) {
       var openBrowser = xmlHttpRequestPUT.getResponseHeader("X-Open-Browser");
       if( openBrowser ) {
         if( sameWindow ) {
@@ -192,29 +194,30 @@ function uploadPDFData(data, errorCallback, sameWindow) {
 
   chrome.storage.sync.get([KEYS.PRINTER_URL,
                            KEYS.OAUTH_CLIENT_ID, KEYS.OAUTH_CLIENT_SECRET,
-                           KEYS.OAUTH_TOKEN_ID, KEYS.OAUTH_TOKEN_SECRET], function(items) {
-    var printer_url = items[KEYS.PRINTER_URL] || DEFAULTS.PRINTER_URL;
-    var clientId = items[KEYS.OAUTH_CLIENT_ID] || "";
-    var clientSecret = items[KEYS.OAUTH_CLIENT_SECRET] || "";
-    var tokenId = items[KEYS.OAUTH_TOKEN_ID] || "";
-    var tokenSecret = items[KEYS.OAUTH_TOKEN_SECRET] || "";
+                           KEYS.OAUTH_TOKEN_ID, KEYS.OAUTH_TOKEN_SECRET],
+                          function(items) {
+                            var printer_url = items[KEYS.PRINTER_URL] || DEFAULTS.PRINTER_URL;
+                            var clientId = items[KEYS.OAUTH_CLIENT_ID] || "";
+                            var clientSecret = items[KEYS.OAUTH_CLIENT_SECRET] || "";
+                            var tokenId = items[KEYS.OAUTH_TOKEN_ID] || "";
+                            var tokenSecret = items[KEYS.OAUTH_TOKEN_SECRET] || "";
 
-    var oauth_header = "OAuth oauth_signature_method=\"PLAINTEXT\"" +
-        ",oauth_consumer_key=\"" + clientId +
-        "\",oauth_token=\"" + tokenId +
-        "\",oauth_signature=\"" + clientSecret + "&" + tokenSecret + "\"";
+                            var oauth_header = "OAuth oauth_signature_method=\"PLAINTEXT\"" +
+                                ",oauth_consumer_key=\"" + clientId +
+                                "\",oauth_token=\"" + tokenId +
+                                "\",oauth_signature=\"" + clientSecret + "&" + tokenSecret + "\"";
 
-    console.log("Sending PDF data to: " + printer_url);
-    xmlHttpRequestPUT.open("PUT", printer_url);
+                            console.log("Sending PDF data to: " + printer_url);
+                            xmlHttpRequestPUT.open("PUT", printer_url);
 
-    if( clientId + "" != "" &&
-        clientSecret + "" != "" &&
-        tokenId + "" != "" &&
-        tokenSecret + "" != "" ) {
+                            if( clientId + "" != "" &&
+                                clientSecret + "" != "" &&
+                                tokenId + "" != "" &&
+                                tokenSecret + "" != "" ) {
 
-        xmlHttpRequestPUT.setRequestHeader("Authorization", oauth_header);
-        console.log("Using Authorization: " + oauth_header);
-    }
-    xmlHttpRequestPUT.send(data);
-  });
+                              xmlHttpRequestPUT.setRequestHeader("Authorization", oauth_header);
+                              console.log("Using Authorization: " + oauth_header);
+                            }
+                            xmlHttpRequestPUT.send(data);
+                          });
 }
--- chrome-scrive-extension/direct_upload.js
+++ chrome-scrive-extension/direct_upload.js
@@ -2,16 +2,16 @@ var inputFile;
 var dropbox;
 
 function handleFiles() {
-    var fileList = this.files;
-    if( fileList.length==1 ) {
-        var file = fileList[0];
+  var fileList = this.files;
+  if( fileList.length==1 ) {
+    var file = fileList[0];
 
-        showUploading();
+    showUploading();
 
-        uploadPDFData(file, function(e) {
-            showError(e);
-        },true);
-    }
+    uploadPDFData(file, function(e) {
+      showError(e);
+    },true);
+  }
 }
 
 function dragenter(e) {
@@ -49,30 +49,30 @@ function preventDrop(e) {
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    // Set up the templateable parts of the modal
-    inputFile = document.querySelector('input[type="file"]');
+  // Set up the templateable parts of the modal
+  inputFile = document.querySelector('input[type="file"]');
 
-    inputFile.addEventListener("change", handleFiles, false);
+  inputFile.addEventListener("change", handleFiles, false);
 
-    dropbox = document.querySelector('.dropbox');
-    overlay = document.querySelector('.dropbox-overlay');
-    overlayText = overlay.querySelector('h1');
-    container = document.querySelector('.container-inner');
-    error = document.querySelector('.error');
+  dropbox = document.querySelector('.dropbox');
+  overlay = document.querySelector('.dropbox-overlay');
+  overlayText = overlay.querySelector('h1');
+  container = document.querySelector('.container-inner');
+  error = document.querySelector('.error');
 
-    document.body.addEventListener("dragenter", dragenter, false);
-    document.body.addEventListener("drop", preventDrop, false);
-    document.body.addEventListener("dragover", preventDrop, false);
-    overlay.addEventListener("dragover", dragover, false);
-    overlay.addEventListener("dragleave", dragleave, false);
-    overlay.addEventListener("drop", drop, false);
+  document.body.addEventListener("dragenter", dragenter, false);
+  document.body.addEventListener("drop", preventDrop, false);
+  document.body.addEventListener("dragover", preventDrop, false);
+  overlay.addEventListener("dragover", dragover, false);
+  overlay.addEventListener("dragleave", dragleave, false);
+  overlay.addEventListener("drop", drop, false);
 
-    overlay.style.width = container.offsetWidth + "px";
-    overlay.style.height = container.offsetHeight + "px";
+  overlay.style.width = container.offsetWidth + "px";
+  overlay.style.height = container.offsetHeight + "px";
 
-    // Both the button and the overlay text are centered, but not exactly.
-    // So we adjust the centering (that we do using line-height) with a few px.
-    overlayText.style.lineHeight = (container.offsetHeight - 4) + "px";
+  // Both the button and the overlay text are centered, but not exactly.
+  // So we adjust the centering (that we do using line-height) with a few px.
+  overlayText.style.lineHeight = (container.offsetHeight - 4) + "px";
 });
 
 var showError = function(errorData) {
@@ -81,9 +81,9 @@ var showError = function(errorData) {
   error.style.display = "block";
   error.innerHTML = "<h1>N&aring;got gick fel!</h1><p>Genom att maila felmeddelandet nedan till <a target='_blank' href='mailto:info@scrive.com?subject=Scrive Chrome-till%C3%A4gg - buggrapport'>info@scrive.com</a> kan du hj&auml;lpa till att g&ouml;ra tj&auml;nsten b&auml;ttre.<br /><br />";
   error.innerHTML += "<p>Felmeddelande:<br/>" +
-                                errorData.headers.join("<br/>") + "<br/>" +
-                                "Status: " + errorData.status + " " + errorData.statusText + "<br/>" +
-                            "</p>";
+    errorData.headers.join("<br/>") + "<br/>" +
+    "Status: " + errorData.status + " " + errorData.statusText + "<br/>" +
+    "</p>";
 
   chrome.storage.sync.get(KEYS.PRINTER_URL, function(items) {
     var printer_url = items[KEYS.PRINTER_URL] || DEFAULTS.PRINTER_URL;
@@ -99,19 +99,19 @@ var showError = function(errorData) {
 };
 
 var showUploading = function() {
-    overlay.classList.remove('visible');
+  overlay.classList.remove('visible');
 
-    container.innerHTML = '<h1 class="uploading">Laddar upp</h1>';
-    var spinner = new Spinner({
-      lines  : 9,     // The number of lines to draw
-        length : 3,     // The length of each line
-        width  : 2,     // The line thickness
-        radius : 5,     // The radius of the inner circle
-        color  : '#000000', // #rbg or #rrggbb
-        speed  : 1.5,    // Rounds per second
-        trail  : 74,     // Afterglow percentage
-        shadow : false,   // Whether to render a shadow
-        className: 'spinner'
-    }).spin();
-    container.appendChild(spinner.el);
+  container.innerHTML = '<h1 class="uploading">Laddar upp</h1>';
+  var spinner = new Spinner({
+    lines  : 9,     // The number of lines to draw
+    length : 3,     // The length of each line
+    width  : 2,     // The line thickness
+    radius : 5,     // The radius of the inner circle
+    color  : '#000000', // #rbg or #rrggbb
+    speed  : 1.5,    // Rounds per second
+    trail  : 74,     // Afterglow percentage
+    shadow : false,   // Whether to render a shadow
+    className: 'spinner'
+  }).spin();
+  container.appendChild(spinner.el);
 };
