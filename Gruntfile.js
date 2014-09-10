96d0a2733b22e2e32c8bd11a2f63c95d14748756
Gracjan Polak <gracjan@scrive.com>
2014-09-10 08:18:42 +0200

    Implement static file serving in grunt:connect

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,8 +21,29 @@ module.exports = function (grunt) {
         // run in parallel with other tasks
         runInBackground: false
       }
+    },
+    connect: {
+      server: {
+        port: 8282,
+        hostname: 'localhost',
+        base: 'tests',
+        middleware: function(connect, options) {
+          // var proxy = require('grunt-connect-proxy/lib/utils').proxyRequest;
+          return [
+            // Include the proxy first
+            // proxy,
+            // Serve static files.
+            connect.static(options.base),
+            // Make empty directories browsable.
+            connect.directory(options.base)
+          ];
+        }
+      },
+      proxies: []
     }
   });
 
+  grunt.loadNpmTasks('grunt-connect');
+  grunt.loadNpmTasks('grunt-connect-proxy');
   grunt.loadNpmTasks('grunt-http-server');
-}
+};
