13b9ba2056826d9b566a97d980768604d7996fe0
Gracjan Polak <gracjan@scrive.com>
2014-09-10 10:52:44 +0200

    Remove http-server (replaced by grunt-connect)

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,23 +6,6 @@ var Busboy = require('busboy');
 module.exports = function (grunt) {
 
   grunt.initConfig({
-    'http-server': {
-      'tests': {
-
-        // the server root directory
-        root: "tests",
-        port: 8282,
-        // port: function() { return 8282; }
-        host: "127.0.0.1",
-        // cache: <sec>,
-        showDir : true,
-        autoIndex: true,
-        // server default file extension
-        ext: "html",
-        // run in parallel with other tasks
-        runInBackground: false
-      }
-    },
     connect: {
       server: {
         hostname: 'localhost',
