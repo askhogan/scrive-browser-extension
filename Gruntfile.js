167ecfc5f496c7ec9591b988f22fb681a1e4cda3
Gracjan Polak <gracjan@scrive.com>
2014-09-10 10:51:56 +0200

    Add POST handling for tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 
 'use strict';
 
+var Busboy = require('busboy');
 
 module.exports = function (grunt) {
 
@@ -24,26 +25,56 @@ module.exports = function (grunt) {
     },
     connect: {
       server: {
-        port: 8282,
         hostname: 'localhost',
-        base: 'tests',
-        middleware: function(connect, options) {
-          // var proxy = require('grunt-connect-proxy/lib/utils').proxyRequest;
-          return [
-            // Include the proxy first
-            // proxy,
-            // Serve static files.
-            connect.static(options.base),
-            // Make empty directories browsable.
-            connect.directory(options.base)
-          ];
+        options: {
+          port: 8282,
+          base: 'tests',
+          keepalive: true,
+          middleware: function(connect, options, middlewares) {
+            function myMiddleware(req, res, next) {
+              if( req.method=="POST" ) {
+                var busboy = new Busboy({ headers: req.headers });
+                var isValid = false;
+                busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+                  //console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
+                  file.on('data', function(data) {
+                    //console.log('File [' + fieldname + '] got ' + data.length + ' bytes');
+                  });
+                  file.on('end', function() {
+                    //console.log('File [' + fieldname + '] Finished');
+                  });
+                });
+                busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated) {
+                  //console.log('Field [' + fieldname + ']: value: ' + val);
+                  if( fieldname=='a_variable' && val=='a_value' ) {
+                    // We expect a variable to be present
+                    isValid = true;
+                  }
+                });
+                busboy.on('finish', function() {
+                  //console.log('Done parsing form!');
+                  if( isValid ) {
+                    res.writeHead(303, { Connection: 'close', Location: '/sales_contract.pdf' });
+                    res.end();
+                  }
+                  else {
+                    res.writeHead(404, { Connection: 'close', 'Content-type': 'text/plain; charset=utf-8', 'Cache-control': 'no-cache' });
+                    res.end('Your POST request should have \'a_variable\' with \'a_value\'. Then you would be let through.');
+                  }
+                });
+                req.pipe(busboy);
+              }
+              else {
+                next();
+              }
+            };
+            middlewares.unshift(myMiddleware);
+            return middlewares;
+          }
         }
-      },
-      proxies: []
+      }
     }
   });
 
-  grunt.loadNpmTasks('grunt-connect');
-  grunt.loadNpmTasks('grunt-connect-proxy');
-  grunt.loadNpmTasks('grunt-http-server');
+  require('load-grunt-tasks')(grunt);
 };
