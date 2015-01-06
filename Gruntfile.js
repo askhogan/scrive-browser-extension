deff843cce38efa6e2f6097a903b0bc6e3fa5811
ernes32 <ernes32@gmail.com>
2015-01-06 21:48:33 +0200

    x Grunt.js CRX build infrastructure    on windows it works with CygWin and OpenSSL
    I have removed bower, crxmake.sh, makefile and bunch of  "devDependencies":
    now we have a clean Grunt only way of building chrome addon.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,63 +1,28 @@
 
 'use strict';
 
-var Busboy = require('busboy');
+module.exports = function(grunt) {
 
-module.exports = function (grunt) {
-
-  grunt.initConfig({
-    connect: {
-      server: {
-        hostname: 'localhost',
-        options: {
-          port: 8282,
-          base: 'tests',
-          keepalive: true,
-          middleware: function(connect, options, middlewares) {
-            function myMiddleware(req, res, next) {
-              if( req.method=="POST" ) {
-                var busboy = new Busboy({ headers: req.headers });
-                var isValid = false;
-                busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
-                  //console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
-                  file.on('data', function(data) {
-                    //console.log('File [' + fieldname + '] got ' + data.length + ' bytes');
-                  });
-                  file.on('end', function() {
-                    //console.log('File [' + fieldname + '] Finished');
-                  });
-                });
-                busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated) {
-                  //console.log('Field [' + fieldname + ']: value: ' + val);
-                  if( fieldname=='a_variable' && val=='a_value' ) {
-                    // We expect a variable to be present
-                    isValid = true;
-                  }
-                });
-                busboy.on('finish', function() {
-                  //console.log('Done parsing form!');
-                  if( isValid ) {
-                    res.writeHead(303, { Connection: 'close', Location: '/sales_contract.pdf' });
-                    res.end();
-                  }
-                  else {
-                    res.writeHead(404, { Connection: 'close', 'Content-type': 'text/plain; charset=utf-8', 'Cache-control': 'no-cache' });
-                    res.end('Your POST request should have \'a_variable\' with \'a_value\'. Then you would be let through.');
-                  }
-                });
-                req.pipe(busboy);
-              }
-              else {
-                next();
-              }
-            };
-            middlewares.unshift(myMiddleware);
-            return middlewares;
-          }
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        manifest: grunt.file.readJSON('chrome-scrive-extension/manifest.json'),
+        crx: {
+            myPublicPackage: {
+                "privateKey": "chrome-scrive-extension.pem",
+                "src": "chrome-scrive-extension/",
+                "dest": "output/<%= pkg.name %>-<%= manifest.version %>-dev.crx"
+            }
         }
-      }
-    }
-  });
+    });
 
-  require('load-grunt-tasks')(grunt);
+  // Load the plugin that provides the "uglify" task.
+grunt.loadNpmTasks('grunt-crx');
+
+grunt.registerTask('default', ['myPublicPackage']);
+
 };
+
+
+
+
+
