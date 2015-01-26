7e0bec99c30e2699fdcf8032fc23e3cd5aced3ca
Ernes Durakovic <ernes32@gmail.com>
2015-01-26 11:22:45 +0200

    (9:59:08) Gracjan Polak: * Reenable testing ** TODO Make a way to test this using files in tests (grunt server) ** TODO Fix the EMBED page so that sales_contract.pdf is visible ** TODO Add OBJECT page (same as EMBED) ** TODO Make page with IFRAME NOT download immediatelly ** TODO Make POST page work

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,8 @@
 
 'use strict';
 
+var Busboy = require('busboy');
+
 module.exports = function(grunt) {
 
     grunt.initConfig({
@@ -12,8 +14,61 @@ module.exports = function(grunt) {
                 "src": "chrome-scrive-extension/",
                 "dest": "output/<%= pkg.name %>-<%= manifest.version %>-dev.crx"
             }
+        },
+        connect: {
+            server: {
+                hostname: 'localhost',
+                options: {
+                    port: 8282,
+                    base: 'tests',
+                    keepalive: true,
+                    middleware: function(connect, options, middlewares) {
+                        function myMiddleware(req, res, next) {
+                            if( req.method=="POST" ) {
+                                var busboy = new Busboy({ headers: req.headers });
+                                var isValid = false;
+                                busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+                                    //console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
+                                    file.on('data', function(data) {
+                                        //console.log('File [' + fieldname + '] got ' + data.length + ' bytes');
+                                    });
+                                    file.on('end', function() {
+                                        //console.log('File [' + fieldname + '] Finished');
+                                    });
+                                });
+                                busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated) {
+                                    //console.log('Field [' + fieldname + ']: value: ' + val);
+                                    if( fieldname=='a_variable' && val=='a_value' ) {
+                                        // We expect a variable to be present
+                                        isValid = true;
+                                    }
+                                });
+                                busboy.on('finish', function() {
+                                    //console.log('Done parsing form!');
+                                    if( isValid ) {
+                                        res.writeHead(303, { Connection: 'close', Location: '/sales_contract.pdf' });
+                                        res.end();
+                                    }
+                                    else {
+                                        res.writeHead(404, { Connection: 'close', 'Content-type': 'text/plain; charset=utf-8', 'Cache-control': 'no-cache' });
+                                        res.end('Your POST request should have \'a_variable\' with \'a_value\'. Then you would be let through.');
+                                    }
+                                });
+                                req.pipe(busboy);
+                            }
+                            else {
+                                next();
+                            }
+                        };
+                        middlewares.unshift(myMiddleware);
+                        return middlewares;
+                    }
+                }
+            }
         }
     });
+
+    require('load-grunt-tasks')(grunt);
 
   // Load the plugin that provides the "uglify" task.
 grunt.loadNpmTasks('grunt-crx');
