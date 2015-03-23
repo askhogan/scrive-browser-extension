82edbce57e31dbbe4600cdc23648bb5dbc5a9003
ernes32 <ernes32@gmail.com>
2015-03-23 09:57:57 +0200

    x Initial after install experience x Notify if Scrive URL is empty x Offer to fill in OAuth credentials when empty x Accept only proper urls for Scrive URL, test if they are really working ones x Check if OAuth is working right away and   x show warning if it does not x bug: after Message/Dialog for file:// pdf and allowed local URL access 	Scrive is not reinjected

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,8 @@ module.exports = function(grunt) {
         manifest: grunt.file.readJSON('chrome-scrive-extension/manifest.json'),
         concat: {
           ScriveChromeContentScriptAll: {
-            src:["chrome-scrive-extension/common/ScriveMain.js",
+            src:["chrome-scrive-extension/oauth.js",
+                 "chrome-scrive-extension/common/ScriveMain.js",
                  "chrome-scrive-extension/common/ScrivePlatform.js",
                  "chrome-scrive-extension/common/ScriveLogUtils.js",
                  "chrome-scrive-extension/chrome/ScriveChromeLogger.js",
@@ -27,7 +28,8 @@ module.exports = function(grunt) {
             dest: 'chrome-scrive-extension/ScriveChromeContentScriptAll.js'
           },
           ScriveChromeContentScriptAllUpdate: {
-            src:["chrome-scrive-extension/common/ScriveMain.js",
+            src:["chrome-scrive-extension/oauth.js",
+              "chrome-scrive-extension/common/ScriveMain.js",
               "chrome-scrive-extension/common/ScrivePlatform.js",
               "chrome-scrive-extension/common/ScriveLogUtils.js",
               "chrome-scrive-extension/chrome/ScriveChromeLogger.js",
@@ -44,7 +46,8 @@ module.exports = function(grunt) {
             dest: 'chrome-scrive-extension/ScriveChromeContentScriptAllUpdate.js'
           },
           ScriveIEContentScriptAll: {
-            src:["chrome-scrive-extension/common/ScriveMain.js",
+            src:["chrome-scrive-extension/oauth.js",
+              "chrome-scrive-extension/common/ScriveMain.js",
               "chrome-scrive-extension/common/ScrivePlatform.js",
               "chrome-scrive-extension/common/ScriveLogUtils.js",
               "chrome-scrive-extension/ie/ScriveIELogger.js",
